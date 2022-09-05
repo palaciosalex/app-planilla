4b57d9ccb88ed314d29fa76b5e8c15c84c3e7888
unknown <davixdmarzo97@gmail.com>
2022-09-04 22:55:09 -0500

    cambio 3

--- resources/views/trabajadores/index.blade.php
+++ resources/views/trabajadores/index.blade.php
@@ -2,5 +2,31 @@
 @section('titulo','Trabajadores')
 @section('page2','active')
 @section('contenido')
-    <p>This is appended to the master sidebar.</p>
+<div class="container">
+<h5 class="text-center">Lista de trabajadores</h5>
+<div class="row">
+    <div class="col-4">
+        <input class="form-control" type="text" placeholder="Buscar">
+    </div>
+    <div class="col-3">
+        <button class="btn btn-success">Agregar</button>
+    </div>
+</div>
+<div class="row">
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">Nombre</th>
+        <th scope="col">DNI</th>
+        <th scope="col">Correo</th>
+        <th scope="col">Ingreso x Hora</th>
+        <th scope="col">Estado</th>
+        <th scope="col">Acciónes</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+    </table>
+</div>
+</div>
 @endsection
